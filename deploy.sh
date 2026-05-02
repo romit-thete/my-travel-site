a3571e6f87ea5d9c62390929aeaa917b4aaf9815
Romit Thete <romit.avishkar@gmail.com>
2026-05-02 23:44:25 +0200

    Certbot installation and configuration of HTTPS on nginx

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,16 @@ docker build -t travel-site /home/ubuntu/app/webapp/.
 echo "Creating network..."
 docker network create my-network
 
+echo "Creating certificates..."
+sudo certbot certonly \
+  --standalone \
+  --non-interactive \
+  --agree-tos \
+  --email test@mail.com \
+  --redirect \
+  --keep-until-expiring \
+  -d "$1".nip.io
+
 echo "Starting app container..."
 docker run -d \
   --name travel-site \
@@ -28,17 +38,20 @@ docker run -d \
   travel-site
 
 echo "Starting nginx proxy..."
-docker run -d -p 80:80 \
+docker run -d -p 80:80 -p 443:443 \
   --name nginx-proxy \
   --network my-network \
-  -v /home/ubuntu/app/nginx.conf:/etc/nginx/nginx.conf:ro \
-  nginx
+  -e DOMAIN="$1" \
+  -v /home/ubuntu/app/nginx.template.conf:/etc/nginx/nginx.template.conf:ro \
+  -v /etc/letsencrypt:/etc/letsencrypt:ro \
+  nginx \
+  /bin/sh -c "envsubst < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf && nginx -g 'daemon off;'"
 
 echo "Waiting for startup..."
 sleep 2
 
 echo "Running health check..."
-curl -f http://localhost/health.html || (echo "Health check failed" && exit 1)
-docker exec travel-site curl -f http://localhost/health.html || (echo "Health check failed" && exit 1)
+curl -f https://$1.nip.io/health.html || (echo "Health check failed" && exit 1)
+docker exec travel-site curl -f https://$1.nip.io/health.html || (echo "Health check failed" && exit 1)
 
 echo "Deployment successful 🚀"
