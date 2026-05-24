f01409464f5e3c269119d3566bc63575c71e0347
Romit Thete <romit.avishkar@gmail.com>
2026-05-24 21:06:24 +0200

    Updates bootstrap.sh with proper steps for docker installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,69 @@
 #!/bin/bash
 
-set -e  # stop on error
+## TODO: Update pipeline to be run only once when a new host is provisioned (maybe as a separate task / trigger)
+
+set -euo pipefail  # stop on error
+
+# Retry logic
+retry() {
+  local retries=5
+  local wait=5
+  local count=1
+
+  until "$@"; do
+    if [ $count -ge $retries ]; then
+      echo "Command failed after $count attempts."
+      return 1
+    fi
+
+    echo "Command failed. Retrying in $wait seconds..."
+    sleep $wait
+
+    count=$((count + 1))
+  done
+}
+
+echo "Uninstalling older docker packages, if any..."
+sudo apt remove -y \
+  docker.io \
+  docker-compose \
+  docker-compose-v2 \
+  docker-doc \
+  podman-docker \
+  containerd \
+  runc || true
 
 echo "Updating system..."
-sudo apt update
+retry sudo apt update
+retry sudo apt upgrade -y
+
+echo "Setting up Docker's apt repository..."
+retry sudo apt install -y ca-certificates curl
+retry sudo install -m 0755 -d /etc/apt/keyrings
+retry sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# shellcheck disable=SC1091
+source /etc/os-release
+CODENAME="${UBUNTU_CODENAME:-$VERSION_CODENAME}"
+
+# Add the repository to Apt sources:
+cat <<EOF | sudo tee /etc/apt/sources.list.d/docker.sources
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $CODENAME
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+retry sudo apt update
 
 echo "Installing Docker..."
-sudo apt install docker.io -y
+retry sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Installing CertBot..."
-sudo apt install certbot -y
+retry sudo apt install -y certbot
 
 echo "Starting Docker..."
 sudo systemctl start docker
@@ -26,5 +80,8 @@ docker --version
 
 echo "CertBot version:"
 certbot --version
+
+echo "Verifying docker installation success..."
+sudo docker run hello-world
 
 echo "Bootstrap complete 🚀"
